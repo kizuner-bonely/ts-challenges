9a8724ec439dcb6fc06febd9251d4cabea3ad0a6
kizuner-bonely <1003306162@qq.com>
2022-06-16 23:47:16 +0800

    feat: 完成 medium-readonly2

--- medium/8-medium-readonly-2/template.ts
+++ medium/8-medium-readonly-2/template.ts
@@ -1,1 +1,4 @@
-type MyReadonly2<T, K> = any
+type MyReadonly2<
+  T extends Record<keyof any, any>,
+  K extends keyof T = keyof T, //! 如果没传入类型或传入错误类型使用 keyof T
+> = Readonly<Pick<T, K>> & Omit<T, K>
