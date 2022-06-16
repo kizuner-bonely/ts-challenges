a1d29ac69f60d8676e279621e00b2a002aef4381
kizuner-bonely <1003306162@qq.com>
2022-06-16 22:21:37 +0800

    feat: 完成 easy-awaited

--- easy/189-easy-awaited/template.ts
+++ easy/189-easy-awaited/template.ts
@@ -1,1 +1,11 @@
-type MyAwaited = any
+type MyAwaited<P extends null | undefined | Record<keyof any, any>> = P extends
+  | null
+  | undefined
+  ? P
+  : P extends object & {
+      then(fulfilled: infer F): any
+    }
+  ? F extends (v: infer V, ...args: any[]) => any
+    ? MyAwaited<V>
+    : never
+  : P
