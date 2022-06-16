15cd7e8a441f4b1d94e658b870871647498128f3
kizuner-bonely <1003306162@qq.com>
2022-06-16 22:32:36 +0800

    feat: 完成 easy-includes

--- easy/898-easy-includes/template.ts
+++ easy/898-easy-includes/template.ts
@@ -1,1 +1,14 @@
-type Includes<T extends readonly any[], U> = any;
+type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rest,
+]
+  ? IsSame<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
+
+type IsSame<A, B> = (<T>() => T extends A ? 1 : 2) extends <T>() => T extends B
+  ? 1
+  : 2
+  ? true
+  : false
