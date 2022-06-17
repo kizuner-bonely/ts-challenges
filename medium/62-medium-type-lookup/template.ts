44a2ca3c32535439f708ad542e68ae3ff0b9578b
kizuner-bonely <1003306162@qq.com>
2022-06-17 22:50:19 +0800

    feat: 完成 medium-lookup

--- medium/62-medium-type-lookup/template.ts
+++ medium/62-medium-type-lookup/template.ts
@@ -1,1 +1,7 @@
-type LookUp<U, T> = any
+type LookUp<U extends LookUpType, T extends string> = U extends U
+  ? T extends U['type']
+    ? U
+    : never
+  : never
+
+type LookUpType = { type: string }
