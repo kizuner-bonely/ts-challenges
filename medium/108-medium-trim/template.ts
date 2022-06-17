6fcf647792ec5e10fa1dd68071eab7627bb29d38
kizuner-bonely <1003306162@qq.com>
2022-06-17 23:07:13 +0800

    feat: 实现 medium-trim

--- medium/108-medium-trim/template.ts
+++ medium/108-medium-trim/template.ts
@@ -1,1 +1,7 @@
-type Trim<S extends string> = any
+type Trim<S extends string> = TrimLeft<STrimRight<S>>
+
+type ReverseStr<S extends string> = S extends `${infer First}${infer Rest}`
+  ? `${ReverseStr<Rest>}${First}`
+  : S
+
+type STrimRight<S extends string> = ReverseStr<TrimLeft<ReverseStr<S>>>
