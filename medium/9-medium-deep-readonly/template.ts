678c7bd998da7d6dc5ef280ce50e2111dba41ed2
kizuner-bonely <1003306162@qq.com>
2022-06-17 21:56:59 +0800

    feat: 完成 medium-deepReadonly

--- medium/9-medium-deep-readonly/template.ts
+++ medium/9-medium-deep-readonly/template.ts
@@ -1,1 +1,7 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T extends Record<keyof any, any>> = {
+  readonly [K in keyof T]: T[K] extends Record<keyof any, any>
+    ? T[K] extends Function
+      ? T[K]
+      : DeepReadonly<T[K]>
+    : T[K]
+}
