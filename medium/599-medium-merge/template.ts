8efbe6b9dbead376aa2ae4c637ebe465f6329a8e
kizuner-bonely <1003306162@qq.com>
2022-06-18 11:41:19 +0800

    feat: 完成 medium-merge

--- medium/599-medium-merge/template.ts
+++ medium/599-medium-merge/template.ts
@@ -1,1 +1,10 @@
-type Merge<F, S> = any;
+type Merge<
+  F extends Record<keyof any, any>,
+  S extends Record<keyof any, any>,
+> = {
+  [K in keyof F | keyof S]: K extends keyof F
+    ? K extends keyof S
+      ? S[K]
+      : F[K]
+    : S[K]
+}
