c992eea00cc39b8721b15f6b470194ebf66d27b0
kizuner-bonely <1003306162@qq.com>
2022-06-18 23:27:40 +0800

    feat: 完成 medium-dropChar

--- medium/2070-medium-drop-char/template.ts
+++ medium/2070-medium-drop-char/template.ts
@@ -1,1 +1,11 @@
-type DropChar<S, C> = any
+type DropChar<
+  S extends string,
+  C extends string,
+  Res extends string = '',
+> = C extends ''
+  ? never
+  : S extends `${infer First}${infer Rest}`
+  ? First extends C
+    ? DropChar<Rest, C, Res>
+    : DropChar<Rest, C, `${Res}${First}`>
+  : Res
