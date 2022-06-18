062389a1a889dc4a7380667d8082eadad1c2a9d7
kizuner-bonely <1003306162@qq.com>
2022-06-18 11:16:24 +0800

    feat: 完成 medium-lengthOfString

--- medium/298-medium-length-of-string/template.ts
+++ medium/298-medium-length-of-string/template.ts
@@ -1,1 +1,16 @@
-type LengthOfString<S extends string> = any
+export type LengthOfString<
+  S extends string,
+  Count extends number = 0,
+> = S extends `${infer _First}${infer Rest}`
+  ? LengthOfString<Rest, Add<Count, 1> & number>
+  : Count
+
+type BuildArr<
+  Len extends number,
+  Res extends unknown[] = [],
+> = Res['length'] extends Len ? Res : BuildArr<Len, [...Res, unknown]>
+
+type Add<A extends number, B extends number> = [
+  ...BuildArr<A>,
+  ...BuildArr<B>,
+]['length']
