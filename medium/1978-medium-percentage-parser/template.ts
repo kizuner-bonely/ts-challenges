a23517a4817f52e6c4d95bab86aa819b96dca371
kizuner-bonely <1003306162@qq.com>
2022-06-18 23:14:24 +0800

    feat: 完成 medium-percentageParser

--- medium/1978-medium-percentage-parser/template.ts
+++ medium/1978-medium-percentage-parser/template.ts
@@ -1,1 +1,14 @@
-type PercentageParser<A extends string> = any
+type PercentageParser<
+  A extends string,
+  Sign extends string = '',
+  Num extends string = '',
+  Percent extends string = '',
+> = A extends `${infer First}${infer Rest}`
+  ? First extends '+' | '-'
+    ? PercentageParser<Rest, `${Sign}${First}`, Num, Percent>
+    : First extends '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+    ? PercentageParser<Rest, Sign, `${Num}${First}`, Percent>
+    : First extends '%'
+    ? PercentageParser<Rest, Sign, Num, `${Percent}${First}`>
+    : never
+  : [Sign, Num, Percent]
