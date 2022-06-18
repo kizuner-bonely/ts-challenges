792e4748a574ebe02d3baa4d13ec0c0eec4a1134
kizuner-bonely <1003306162@qq.com>
2022-06-18 18:00:47 +0800

    feat: 完成 medium-diff

--- medium/645-medium-diff/template.ts
+++ medium/645-medium-diff/template.ts
@@ -1,1 +1,9 @@
-type Diff<O, O1> = any
+type Diff<O extends Record<string, any>, O1 extends Record<string, any>> = {
+  [K in keyof O | keyof O1 as UnionIncludes<keyof O, K> extends true
+    ? UnionIncludes<keyof O1, K> extends true
+      ? never
+      : K
+    : K]: K extends keyof O ? O[K] : K extends keyof O1 ? O1[K] : never
+}
+
+type UnionIncludes<T, U> = U extends T ? true : false
