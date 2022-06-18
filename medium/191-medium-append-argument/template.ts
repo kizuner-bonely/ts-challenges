b41703959218fe01feb6ddd9ff6069491f9717ea
kizuner-bonely <1003306162@qq.com>
2022-06-18 10:52:20 +0800

    feat: 完成 medium-appendArgument

--- medium/191-medium-append-argument/template.ts
+++ medium/191-medium-append-argument/template.ts
@@ -1,1 +1,5 @@
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends (...args: any[]) => any, A> = Fn extends (
+  ...args: infer Args
+) => infer R
+  ? (...args: [...Args, A]) => R
+  : never
