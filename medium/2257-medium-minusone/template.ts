acb1721d2147520e2c488127e5c67005b64d658e
kizuner-bonely <1003306162@qq.com>
2022-06-18 23:47:58 +0800

    feat: 完成 medium-minusOne

--- medium/2257-medium-minusone/template.ts
+++ medium/2257-medium-minusone/template.ts
@@ -1,1 +1,17 @@
-type MinusOne<T extends number> = any
+//? 这种解法无法通过 1101
+// type MinusOne<T extends number, Res extends any[] = []> = [
+//   ...Res,
+//   '',
+// ]['length'] extends T
+//   ? Res['length']
+//   : MinusOne<T, [...Res, '']>
+
+type MinusOne<T extends number, A extends any[] = []> = [
+  ...A,
+  any,
+  any,
+]['length'] extends T
+  ? [...A, any]['length']
+  : [...A, any]['length'] extends T
+  ? A['length']
+  : MinusOne<T, [...A, any, any]>
