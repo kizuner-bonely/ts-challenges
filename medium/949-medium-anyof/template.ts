ca504260d3be682a7f845d51330a12cea7db2945
kizuner-bonely <1003306162@qq.com>
2022-06-18 22:41:39 +0800

    feat: 完成 medium-anyof

--- medium/949-medium-anyof/template.ts
+++ medium/949-medium-anyof/template.ts
@@ -1,1 +1,13 @@
-type AnyOf<T extends readonly any[]> = any;
+type AnyOf<T extends readonly any[]> = T extends [infer First, ...infer Rest]
+  ? First extends false | 0 | undefined | null | never | ''
+    ? AnyOf<Rest>
+    : First extends any[]
+    ? First['length'] extends 0
+      ? AnyOf<Rest>
+      : true
+    : First extends Record<keyof any, any>
+    ? [keyof First] extends [never]
+      ? AnyOf<Rest>
+      : true
+    : true
+  : false
