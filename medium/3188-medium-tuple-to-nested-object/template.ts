5aeb95a7f1b47efc8a8cbc033e1a35bbc4b6d73f
kizuner-bonely <1003306162@qq.com>
2022-06-19 15:47:37 +0800

    feat: 完成 medium-tupleToNestedObject

--- medium/3188-medium-tuple-to-nested-object/template.ts
+++ medium/3188-medium-tuple-to-nested-object/template.ts
@@ -1,1 +1,6 @@
-type TupleToNestedObject<T, U> = any
+type TupleToNestedObject<T extends any[], U> = T extends [
+  infer First,
+  ...infer Rest,
+]
+  ? { [K in First & keyof any]: TupleToNestedObject<Rest, U> }
+  : U
