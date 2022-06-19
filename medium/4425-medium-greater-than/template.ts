a14d815ff8c15b2a3eda7c3c2626bd797fa2e1e5
kizuner-bonely <1003306162@qq.com>
2022-06-19 17:31:27 +0800

    feat: 完成 medium-greaterThan

--- medium/4425-medium-greater-than/template.ts
+++ medium/4425-medium-greater-than/template.ts
@@ -1,1 +1,19 @@
-type GreaterThan<T extends number, U extends number> = any
+type GreaterThan<
+  T extends number,
+  U extends number,
+  Count extends number = 0,
+> = Count extends T
+  ? false
+  : Count extends U
+  ? true
+  : GreaterThan<T, U, Add<Count, 1> & number>
+
+type BuildArr<
+  Len extends number,
+  Res extends any[] = [],
+> = Res['length'] extends Len ? Res : BuildArr<Len, [...Res, 0]>
+
+type Add<A extends number, B extends number> = [
+  ...BuildArr<A>,
+  ...BuildArr<B>,
+]['length']
