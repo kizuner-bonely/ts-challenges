9dddf83571f62d96533bb32a7b6ac6236d9ae0cf
kizuner-bonely <1003306162@qq.com>
2022-06-19 17:58:49 +0800

    feat: 完成 medium-chunk

--- medium/4499-medium-chunk/template.ts
+++ medium/4499-medium-chunk/template.ts
@@ -1,1 +1,12 @@
-type Chunk = any
+type Chunk<
+  A extends any[],
+  L extends number,
+  Selected extends any[] = [],
+  Res extends any[] = [],
+> = A extends [infer First, ...infer Rest]
+  ? Selected['length'] extends L
+    ? Chunk<A, L, [], [...Res, Selected]>
+    : Chunk<Rest, L, [...Selected, First], Res>
+  : Selected['length'] extends 0
+  ? Res
+  : [...Res, Selected]
