d486f77e4d5de6ed8d2e6ca655c7ddac2c61ec7e
kizuner-bonely <1003306162@qq.com>
2022-06-19 21:56:19 +0800

    feat: 完成 medium-unique

--- medium/5360-medium-unique/template.ts
+++ medium/5360-medium-unique/template.ts
@@ -1,1 +1,8 @@
-type Unique<T> = any
+type Unique<T extends any[], Res extends any[] = []> = T extends [
+  infer First,
+  ...infer Rest,
+]
+  ? Includes<Res, First> extends true
+    ? Unique<Rest, Res>
+    : Unique<Rest, [...Res, First]>
+  : Res
