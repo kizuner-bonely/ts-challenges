ecbc702551f3ffe79f6dc6c0151b254d5cb58c66
kizuner-bonely <1003306162@qq.com>
2022-06-19 21:34:20 +0800

    feat: 完成 medium-trunc

--- medium/5140-medium-trunc/template.ts
+++ medium/5140-medium-trunc/template.ts
@@ -1,1 +1,10 @@
-type Trunc = any
+type Trunc<
+  T extends number | string,
+  Res extends string = '',
+> = `${T}` extends `${infer S}`
+  ? S extends `${infer First}${infer Rest}`
+    ? First extends '.'
+      ? Res
+      : Trunc<Rest, `${Res}${First}`>
+    : Res
+  : never
