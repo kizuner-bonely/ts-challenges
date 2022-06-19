601b1db7798ebbe62386a940fcdfd1053b1f16cb
kizuner-bonely <1003306162@qq.com>
2022-06-19 16:49:54 +0800

    feat: 完成 medium-bemStyleString

--- medium/3326-medium-bem-style-string/template.ts
+++ medium/3326-medium-bem-style-string/template.ts
@@ -1,1 +1,13 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+export type BEM<
+  B extends string,
+  E extends string[],
+  M extends string[],
+> = HasEle<E> extends true
+  ? HasEle<M> extends true
+    ? `${B}__${E[number]}--${M[number]}`
+    : `${B}__${E[number]}`
+  : HasEle<M> extends true
+  ? `${B}--${M[number]}`
+  : B
+
+type HasEle<A extends any[]> = A['length'] extends 0 ? false : true
--- medium/3326-medium-bem-style-string/test-cases.ts
+++ medium/3326-medium-bem-style-string/test-cases.ts
@@ -1,7 +1,18 @@
 import { Equal, Expect } from '@type-challenges/utils'
+import type { BEM } from './template'
 
 type cases = [
   Expect<Equal<BEM<'btn', ['price'], []>, 'btn__price'>>,
-  Expect<Equal<BEM<'btn', ['price'], ['warning', 'success']>, 'btn__price--warning' | 'btn__price--success' >>,
-  Expect<Equal<BEM<'btn', [], ['small', 'medium', 'large']>, 'btn--small' | 'btn--medium' | 'btn--large' >>,
+  Expect<
+    Equal<
+      BEM<'btn', ['price'], ['warning', 'success']>,
+      'btn__price--warning' | 'btn__price--success'
+    >
+  >,
+  Expect<
+    Equal<
+      BEM<'btn', [], ['small', 'medium', 'large']>,
+      'btn--small' | 'btn--medium' | 'btn--large'
+    >
+  >,
 ]
