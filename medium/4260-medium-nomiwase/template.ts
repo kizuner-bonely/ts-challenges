b3ae28439f1d1abf3309cbd05517219f01e2ac9c
kizuner-bonely <1003306162@qq.com>
2022-06-19 17:24:17 +0800

    feat: 完成 medium-nomiwase

--- medium/4260-medium-nomiwase/template.ts
+++ medium/4260-medium-nomiwase/template.ts
@@ -1,1 +1,17 @@
-type AllCombinations<S> = any
+type AllCombinations<S extends string> = [S] extends ['']
+  ? ''
+  : AllCombinationsLoop<TransStrToUnion<S>>
+
+type AllCombinationsLoop<A extends string, B extends string = A> = B extends B
+  ? Combine<B, AllCombinations<Exclude<A, B>>>
+  : never
+
+type TransStrToUnion<S extends string> = S extends `${infer First}${infer Rest}`
+  ? First | TransStrToUnion<Rest>
+  : never
+
+type Combine<A extends string, B extends string> =
+  | A
+  | B
+  | `${A}${B}`
+  | `${B}${A}`
