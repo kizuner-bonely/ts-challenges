ed4f775df6be7730fcabde08a27eca923fb0aa93
kizuner-bonely <1003306162@qq.com>
2022-06-19 21:38:26 +0800

    feat: 完成 medium-indexOf

--- medium/5153-medium-indexof/template.ts
+++ medium/5153-medium-indexof/template.ts
@@ -1,1 +1,9 @@
-type IndexOf<T, U> = any
+type IndexOf<
+  T extends any[],
+  U extends number,
+  Ind extends number[] = [],
+> = T extends [infer First, ...infer Rest]
+  ? First extends U
+    ? Ind['length']
+    : IndexOf<Rest, U, [...Ind, 0]>
+  : -1
