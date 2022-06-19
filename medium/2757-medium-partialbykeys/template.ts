b769a5d0b316f805a416a8d46f3364f91f72596a
kizuner-bonely <1003306162@qq.com>
2022-06-19 14:11:19 +0800

    feat: 完成 medium-partialByKeys

--- medium/2757-medium-partialbykeys/template.ts
+++ medium/2757-medium-partialbykeys/template.ts
@@ -1,1 +1,12 @@
-type PartialByKeys<T , K> = any
+type PartialByKeys<
+  T extends Record<string, any>,
+  K extends string = keyof T & string,
+> = Copy<
+  Partial<Pick<T, K & keyof T>> & {
+    [Key in keyof T as Key extends K ? never : Key]: T[Key]
+  }
+>
+
+type Copy<T> = {
+  [K in keyof T]: T[K]
+}
