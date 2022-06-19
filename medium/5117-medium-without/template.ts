c98c099c1b5249c82ef9c799115adb77e3f78511
kizuner-bonely <1003306162@qq.com>
2022-06-19 21:31:26 +0800

    feat: 完成 medium-without

--- medium/5117-medium-without/template.ts
+++ medium/5117-medium-without/template.ts
@@ -1,1 +1,22 @@
-type Without<T, U> = any
+type Without<
+  T extends any[],
+  U extends number | number[],
+  Res extends any[] = [],
+> = T extends [infer First, ...infer Rest]
+  ? U extends number[]
+    ? ArrIncludes<U, First & number> extends true
+      ? Without<Rest, U, Res>
+      : Without<Rest, U, [...Res, First]>
+    : First extends U
+    ? Without<Rest, U, Res>
+    : Without<Rest, U, [...Res, First]>
+  : Res
+
+type ArrIncludes<Arr extends any[], Ele extends number> = Arr extends [
+  infer First,
+  ...infer Rest,
+]
+  ? First extends Ele
+    ? true
+    : ArrIncludes<Rest, Ele>
+  : false
