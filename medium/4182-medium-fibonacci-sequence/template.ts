622c55eef3cf0702d78872d4c9fd27820b826001
kizuner-bonely <1003306162@qq.com>
2022-06-19 17:05:41 +0800

    feat: 完成 medium-fibonacciSequence

--- medium/4182-medium-fibonacci-sequence/template.ts
+++ medium/4182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,8 @@
-type Fibonacci<T extends number> = any
+type Fibonacci<
+  T extends number,
+  Pre extends any[] = [0],
+  Cur extends any[] = [],
+  Ind extends any[] = [],
+> = Ind['length'] extends T
+  ? Cur['length']
+  : Fibonacci<T, Cur, [...Pre, ...Cur], [...Ind, 0]>
