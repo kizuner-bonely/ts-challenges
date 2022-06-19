e2e719a2af31d866105b4f431b5b50ecfe5b30f9
kizuner-bonely <1003306162@qq.com>
2022-06-19 15:43:49 +0800

    feat: 完成 medium-objectEntries

--- medium/2946-medium-objectentries/template.ts
+++ medium/2946-medium-objectentries/template.ts
@@ -1,1 +1,5 @@
-type ObjectEntries<T> = any
+type ObjectEntries<T extends Record<keyof any, any>> = {
+  [K in keyof T]-?: [K, RemoveUndefined<T[K]>]
+}[keyof T]
+
+type RemoveUndefined<T> = [T] extends [undefined] ? T : Exclude<T, undefined>
