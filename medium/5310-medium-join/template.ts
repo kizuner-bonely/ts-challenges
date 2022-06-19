95df0edef211b0743809f9c22b80cbc6787c1186
kizuner-bonely <1003306162@qq.com>
2022-06-19 21:49:11 +0800

    feat: 完成 medium-join

--- medium/5310-medium-join/template.ts
+++ medium/5310-medium-join/template.ts
@@ -1,1 +1,10 @@
-type Join<T, U> = any
+type Join<
+  T extends any[],
+  U extends number | string,
+  Res extends string = '',
+> = T extends [infer First, ...infer Rest]
+  ? Join<Rest, U, `${Res}${First & string}${U}`>
+  : RemoveLastEle<Res>
+
+type RemoveLastEle<S extends string> =
+  ReverseStr<S> extends `${infer _First}${infer Str}` ? ReverseStr<Str> : S
