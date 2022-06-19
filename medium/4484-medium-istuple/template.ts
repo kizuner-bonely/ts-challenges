3c5030c1d6a47bfd724da56e9c35463ed6fc69fd
kizuner-bonely <1003306162@qq.com>
2022-06-19 17:41:13 +0800

    feat: 完成 medium-isTuple

--- medium/4484-medium-istuple/template.ts
+++ medium/4484-medium-istuple/template.ts
@@ -1,1 +1,9 @@
-type IsTuple<T> = any
+type IsTuple<T> = T extends readonly [...params: infer Eles]
+  ? NotEqual<Eles['length'], number>
+  : false
+
+type NotEqual<A, B> = (<T>() => T extends A ? 1 : 2) extends <
+  T,
+>() => T extends B ? 1 : 2
+  ? false
+  : true
