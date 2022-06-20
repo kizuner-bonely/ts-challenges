a0163aaa6775429b6c5d2dc064a5b29b833d5000
kizuner-bonely <1003306162@qq.com>
2022-06-20 23:55:25 +0800

    feat: 完成 hard-filter

--- hard/399-hard-tuple-filter/template.ts
+++ hard/399-hard-tuple-filter/template.ts
@@ -1,1 +1,8 @@
-type FilterOut<T extends any[], F> = any
+type FilterOut<T extends any[], F, Res extends any[] = []> = T extends [
+  infer First,
+  ...infer Rest,
+]
+  ? [First] extends [F]
+    ? FilterOut<Rest, F, Res>
+    : FilterOut<Rest, F, [...Res, First]>
+  : Res
