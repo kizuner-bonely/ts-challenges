a7052ac81a262c3344113c1d0ce3a10b1bea35f0
kizuner-bonely <1003306162@qq.com>
2022-06-20 21:31:32 +0800

    feat: 完成 hard-currying1

--- hard/17-hard-currying-1/template.ts
+++ hard/17-hard-currying-1/template.ts
@@ -1,1 +1,6 @@
-declare function Currying(fn: any): any
+declare function Currying<T>(fn: T): 
+  T extends (...args: infer A) => infer R
+    ? A extends [infer First, ...infer Rest]
+      ? (x: First) => ReturnType<typeof Currying<(...args: Rest) => R>>
+      : R
+    : never
