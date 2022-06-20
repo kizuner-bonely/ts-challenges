1371ccf9ae907aad960bdb38b6117d45b9130d3c
kizuner-bonely <1003306162@qq.com>
2022-06-20 23:30:41 +0800

    feat: 完成 hard-typedGet

--- hard/270-hard-typed-get/template.ts
+++ hard/270-hard-typed-get/template.ts
@@ -1,1 +1,7 @@
-type Get<T, K> = string
+type Get<T extends Record<string, any>, K> = K extends `${infer A}.${infer B}`
+  ? A extends keyof T
+    ? Get<T[A], B>
+    : never
+  : K extends keyof T
+  ? T[K]
+  : never
