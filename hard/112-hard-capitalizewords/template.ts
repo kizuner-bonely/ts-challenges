c410fcd59ad911a014b553de2f23c2818e75e5ff
kizuner-bonely <1003306162@qq.com>
2022-06-20 21:59:21 +0800

    feat: 完成 hard-capitalizeWords

--- hard/112-hard-capitalizewords/template.ts
+++ hard/112-hard-capitalizewords/template.ts
@@ -1,1 +1,11 @@
-type CapitalizeWords<S extends string> = any
+type CapitalizeWords<
+  S extends string,
+  T extends boolean = true,
+  Res extends string = '',
+> = S extends `${infer First}${infer Rest}`
+  ? T extends true
+    ? CapitalizeWords<Rest, false, `${Res}${Uppercase<First>}`>
+    : First extends ' ' | '.' | ','
+    ? CapitalizeWords<Rest, true, `${Res}${First}`>
+    : CapitalizeWords<Rest, false, `${Res}${First}`>
+  : Res
