08ac6e185e1c7adf965a98f85110067f54ba099e
kizuner-bonely <1003306162@qq.com>
2022-06-20 23:19:30 +0800

    feat: 完成 hard-vueBasicProps

--- hard/213-hard-vue-basic-props/template.ts
+++ hard/213-hard-vue-basic-props/template.ts
@@ -1,1 +1,29 @@
-declare function VueBasicProps(options: any): any
+declare function VueBasicProps<P, D, C, M>(
+  options: VueInstancePlus<P, D, C, M>,
+): any
+
+type VueInstancePlus<P, D, C, M> = {
+  props: P
+  data(this: Props<P>): D
+  computed: C
+  methods: M
+} & ThisType<
+  Props<P> &
+    D & {
+      [K in keyof C]: C[K] extends () => infer R ? R : any
+    } & M
+>
+
+type TupleToUnionPlus<T> = T extends (infer A)[] ? A : T
+
+type Props<P> = {
+  [K in keyof P]: MyReturnTypePlus<
+    TupleToUnionPlus<P[K] extends { type: infer Type } ? Type : P[K]>
+  >
+}
+
+type MyReturnTypePlus<T> = T extends () => infer A
+  ? A
+  : T extends new (...args: any[]) => infer A
+  ? A
+  : any
