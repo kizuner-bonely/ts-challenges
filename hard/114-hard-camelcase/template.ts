2f0265f58cc7f3e09c5ee3f0d836fc9f8e373a02
kizuner-bonely <1003306162@qq.com>
2022-06-20 22:03:09 +0800

    feat: 完成 hard-camelCase

--- hard/114-hard-camelcase/template.ts
+++ hard/114-hard-camelcase/template.ts
@@ -1,1 +1,11 @@
-type CamelCase<S extends string> = any
+type CamelCase<
+  S extends string,
+  T extends boolean = false,
+  Res extends string = '',
+> = S extends `${infer First}${infer Rest}`
+  ? T extends true
+    ? CamelCase<Rest, false, `${Res}${Uppercase<First>}`>
+    : First extends '_'
+    ? CamelCase<Rest, true, `${Res}`>
+    : CamelCase<Rest, false, `${Res}${Lowercase<First>}`>
+  : Res
