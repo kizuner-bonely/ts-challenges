41b4b6482381b5400bd485a8c2a0a7399d0ab6b2
kizuner-bonely <1003306162@qq.com>
2022-06-21 23:45:33 +0800

    feat: 完成 hard-stringJoin

--- hard/847-hard-string-join/template.ts
+++ hard/847-hard-string-join/template.ts
@@ -1,1 +1,13 @@
-declare function join(delimiter: any): (...parts: any[]) => any;
+declare function join<D extends string>(
+  delimiter: D,
+): <P extends string[]>(...parts: P) => GetJoinType<D, P>
+
+type GetJoinType<D extends string, P extends string[], Res extends string = ''> =
+  P extends [infer First extends string, ...infer Rest extends string[]]
+    ? GetJoinType<D, Rest, `${Res}${First}${D}`>
+    : RemoveLastEleByCondition<Res, D>
+
+type RemoveLastEleByCondition<S extends string, D extends string> =
+  ReverseStr<S> extends `${D}${infer Rest}`
+    ? ReverseStr<Rest>
+    : S
