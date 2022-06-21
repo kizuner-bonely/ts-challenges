182104015191c3b17bdf59187667c0bf42042c18
kizuner-bonely <1003306162@qq.com>
2022-06-21 23:30:58 +0800

    feat: 完成 hard-unionToTuple

--- hard/730-hard-union-to-tuple/template.ts
+++ hard/730-hard-union-to-tuple/template.ts
@@ -1,1 +1,11 @@
-type UnionToTuple<T> = any
+export type UnionToTuple<T> = UnionToIntersection<
+  T extends any ? () => T : never
+> extends () => infer R
+  ? [...UnionToTuple<Exclude<T, R>>, R]
+  : []
+
+type UnionToIntersection<U> = (U extends U ? (x: U) => any : never) extends (
+  x: infer R,
+) => any
+  ? R
+  : never
