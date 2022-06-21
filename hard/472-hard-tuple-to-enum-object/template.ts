36d80d4f18dfe632adc262a587e10a219cfaf39e
kizuner-bonely <1003306162@qq.com>
2022-06-21 22:13:16 +0800

    feat: 完成 hard-tupleToEnumObject

--- hard/472-hard-tuple-to-enum-object/template.ts
+++ hard/472-hard-tuple-to-enum-object/template.ts
@@ -1,1 +1,10 @@
-type Enum<T extends readonly string[], N extends boolean = false> = any
+type Enum<T extends readonly string[], N extends boolean = false> = {
+  readonly [K in T[number] as Capitalize<K>]: N extends true ? GetIndex<T, K> : K
+}
+
+type GetIndex<T extends readonly string[], K extends T[number], U extends unknown[] = []> =
+  T extends readonly [infer First, ...infer Rest extends string[]]
+    ? K extends First
+      ? U['length']
+      : GetIndex<Rest, K, [...U, unknown]>
+    : U['length']
