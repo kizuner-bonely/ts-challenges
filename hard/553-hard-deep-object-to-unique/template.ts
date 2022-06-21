31b05c430f9d646fb099bce84b8a206c129991a0
kizuner-bonely <1003306162@qq.com>
2022-06-21 23:17:03 +0800

    feat: 完成 hard-deepObjectToUnique

--- hard/553-hard-deep-object-to-unique/template.ts
+++ hard/553-hard-deep-object-to-unique/template.ts
@@ -1,1 +1,5 @@
-type DeepObjectToUniq<O extends object> = any
+type DeepObjectToUniq<O extends object> = {
+  [K in keyof O]: O[K] extends Record<keyof any, any>
+    ? DeepObjectToUniq<O[K] & { _?: [O, K] }>
+    : O[K]
+}
