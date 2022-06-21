4176621fafcd9df62b07b5fa2da3b541e27a5f67
kizuner-bonely <1003306162@qq.com>
2022-06-21 23:05:46 +0800

    feat: 完成 hard-printf

--- hard/545-hard-printf/template.ts
+++ hard/545-hard-printf/template.ts
@@ -1,1 +1,10 @@
-type Format<T extends string> = any
+type Format<T extends string> = T extends `${string}%${infer A}${infer B}`
+  ? A extends keyof NewControlsMap
+    ? (x: NewControlsMap[A]) => Format<B>
+    : Format<B>
+  : string
+
+type NewControlsMap = {
+  s: string
+  d: number
+}
