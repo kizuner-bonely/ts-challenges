297aaa0b799efd594dabd6905361ce3a18c559e2
kizuner-bonely <1003306162@qq.com>
2022-06-22 23:45:28 +0800

    feat: 完成 hard-mutableKeys

--- hard/5181-hard-mutable-keys/template.ts
+++ hard/5181-hard-mutable-keys/template.ts
@@ -1,1 +1,5 @@
-type MutableKeys<T> = any
+type MutableKeys<T extends Record<keyof any, any>> = keyof {
+  [K in keyof T as IsSame<Pick<T, K>, Readonly<Pick<T, K>>> extends true
+    ? never
+    : K]: T[K]
+}
