a3d3602d300cfaca890a1e15cee4826876eb7504
kizuner-bonely <1003306162@qq.com>
2022-06-22 21:33:33 +0800

    feat: 完成 hard-deepPick

--- hard/956-hard-deeppick/template.ts
+++ hard/956-hard-deeppick/template.ts
@@ -1,1 +1,10 @@
-type DeepPick = any
+type DeepPick<
+  Obj extends Record<string, any>,
+  Key extends string,
+> = UnionToIntersection<
+  Key extends `${infer First}.${infer Rest}`
+    ? UnionToIntersection<{ [K in First]: DeepPick<Obj[K & keyof Obj], Rest> }>
+    : Key extends keyof Obj
+    ? { [K in Key & keyof Obj]: Obj[K] }
+    : never
+>
