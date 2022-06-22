e6c3f4d5180ad02028da0a03041651e8bef7337e
kizuner-bonely <1003306162@qq.com>
2022-06-22 23:12:13 +0800

    feat: 完成 hard-isRequiredKey

--- hard/2857-hard-isrequiredkey/template.ts
+++ hard/2857-hard-isrequiredkey/template.ts
@@ -1,1 +1,9 @@
-type IsRequiredKey<T, K extends keyof T> = any
+// ? 下面这个结果在第3个 case 结果为 boolean
+// type IsRequiredKey<T, K extends keyof T> = K extends keyof GetRequired<T>
+//   ? true
+//   : false
+
+type IsRequiredKey<T, K extends keyof T> = IsSame<
+  Pick<T, K>,
+  { [Key in K]-?: T[Key] } //* 原来为必选的去掉，可选的变成必选的
+>
--- hard/2857-hard-isrequiredkey/test-cases.ts
+++ hard/2857-hard-isrequiredkey/test-cases.ts
@@ -1,7 +1,13 @@
 import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
-    Expect<Equal<IsRequiredKey<{ a: number, b?: string },'a'>, true>>,
-    Expect<Equal<IsRequiredKey<{ a: number, b?: string },'b'>, false>>,
-    Expect<Equal<IsRequiredKey<{ a: number, b?: string },'b' | 'a'>, false>>,
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'a'>, true>>,
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'b'>, false>>,
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'b' | 'a'>, false>>,
 ]
+
+type TT = IsRequiredKey<{ a: number; b?: string }, 'a' | 'b'>
+
+type Test = 'a' | 'b' extends GetRequired<{ a: number; b?: string }>
+  ? true
+  : false
