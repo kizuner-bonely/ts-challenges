3a59684f7ab78e2365e92e7e623b2d5cff85494d
kizuner-bonely <1003306162@qq.com>
2022-06-23 00:04:05 +0800

    feat: hard-intersection

--- hard/5423-hard-intersection/template.ts
+++ hard/5423-hard-intersection/template.ts
@@ -1,1 +1,12 @@
-type Intersection<T> = any
+type Intersection<T extends Array<number | number[]>> = T extends [
+  infer First,
+  ...infer Rest extends Array<number | number[]>,
+]
+  ? First extends unknown[]
+    ? First[number] & Intersection<Rest>
+    : First & Intersection<Rest>
+  : unknown
+
+type A = [1, 2]
+type B = [1, 2, 3]
+type Test = A[number] & B[number] & unknown
--- hard/5423-hard-intersection/test-cases.ts
+++ hard/5423-hard-intersection/test-cases.ts
@@ -6,5 +6,5 @@ type cases = [
   Expect<Equal<Intersection<[[1, 2], [3, 4], [5, 6]]>, never>>,
   Expect<Equal<Intersection<[[1, 2, 3], [2, 3, 4], 3]>, 3>>,
   Expect<Equal<Intersection<[[1, 2, 3], 2 | 3 | 4, 2 | 3]>, 2 | 3>>,
-  Expect<Equal<Intersection<[[1, 2, 3], 2, 3]>, never>>
+  Expect<Equal<Intersection<[[1, 2, 3], 2, 3]>, never>>,
 ]
