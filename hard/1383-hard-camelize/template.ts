460576e93c26f4a2781b565afdffcefc81fc09cb
kizuner-bonely <1003306162@qq.com>
2022-06-22 22:05:56 +0800

    feat: 完成 hard-camelize

--- hard/1383-hard-camelize/template.ts
+++ hard/1383-hard-camelize/template.ts
@@ -1,1 +1,9 @@
-type Camelize<T> = any
+type Camelize<T extends Record<string, any>> = {
+  [K in keyof T as CamelizeStr<K & string>]: T[K] extends Record<string, any>
+    ? Camelize<T[K]>
+    : T[K]
+}
+
+type CamelizeStr<S extends string> = S extends `${infer Left}_${infer Right}`
+  ? `${Left}${Capitalize<Right>}`
+  : S
