78b073bc16664d1a1550ef953029304f08a068db
kizuner-bonely <1003306162@qq.com>
2022-06-22 22:13:15 +0800

    feat: 完成 hard-dropString

--- hard/2059-hard-drop-string/template.ts
+++ hard/2059-hard-drop-string/template.ts
@@ -1,1 +1,9 @@
-type DropString<S, R> = any
+type DropString<
+  S extends string,
+  R extends string,
+  Res extends string = '',
+> = S extends `${infer First}${infer Rest}`
+  ? First extends StringToUnion<R>
+    ? DropString<Rest, R, Res>
+    : DropString<Rest, R, `${Res}${First}`>
+  : Res
