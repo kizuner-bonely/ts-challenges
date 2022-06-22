0921145eea5cafb808f7a9374712d827785db7b3
kizuner-bonely <1003306162@qq.com>
2022-06-22 22:26:04 +0800

    feat: 完成 hard-split

--- hard/2822-hard-split/template.ts
+++ hard/2822-hard-split/template.ts
@@ -1,1 +1,20 @@
-type Split<S extends string, SEP extends string> = any
+type Split<
+  S extends string,
+  SEP extends string,
+  Res extends string[] = [],
+> = IsSame<S, string> extends true
+  ? string[]
+  : SEP extends ''
+  ? S extends ''
+    ? []
+    : SplitEverything<S>
+  : S extends `${infer First}${SEP}${infer Rest}`
+  ? Split<Rest, SEP, [...Res, First]>
+  : [...Res, S]
+
+type SplitEverything<
+  S extends string,
+  Res extends string[] = [],
+> = S extends `${infer First}${infer Rest}`
+  ? SplitEverything<Rest, [...Res, First]>
+  : Res
