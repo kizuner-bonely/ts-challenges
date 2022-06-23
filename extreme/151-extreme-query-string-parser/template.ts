9224e2ef30e025809ed47b59e86428151cc0c5b3
kizuner-bonely <1003306162@qq.com>
2022-06-23 22:12:39 +0800

    feat: 完成 extreme-quertStringParser

--- extreme/151-extreme-query-string-parser/template.ts
+++ extreme/151-extreme-query-string-parser/template.ts
@@ -1,1 +1,28 @@
-type ParseQueryString = any
+type ParseQueryString<S extends string> = S extends ''
+  ? {}
+  : S extends `${infer Param}&${infer Rest}`
+  ? MergeParams<ParseEquals<Param>, ParseQueryString<Rest>>
+  : ParseEquals<S>
+
+type MergeParams<
+  A extends Record<string, any>,
+  B extends Record<string, any>,
+> = {
+  [K in keyof A | keyof B]: K extends keyof A
+    ? K extends keyof B
+      ? MergeValues<A[K], B[K]>
+      : A[K]
+    : K extends keyof B
+    ? B[K]
+    : never
+}
+
+type MergeValues<A, B> = A extends B
+  ? A
+  : B extends unknown[]
+  ? [A, ...B]
+  : [A, B]
+
+type ParseEquals<S extends string> = S extends `${infer Left}=${infer Right}`
+  ? { [K in Left]: Right }
+  : { [K in S]: true }
