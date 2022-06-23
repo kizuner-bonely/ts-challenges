c741308b2c3aed880bf21a092982573379ab7c80
kizuner-bonely <1003306162@qq.com>
2022-06-23 23:32:33 +0800

    feat: 完成 extreme-currying2

--- extreme/462-extreme-currying-2/template.ts
+++ extreme/462-extreme-currying-2/template.ts
@@ -1,1 +1,21 @@
-declare function DynamicParamsCurrying(fn: any): any
+declare function DynamicParamsCurrying<F extends (...args: any) => any>(
+  fn: F,
+): CurriedType<ReturnType<F>, Parameters<F>>
+
+type CurriedType<Ret, Params, Current = []> = Params extends [
+  ...infer Rest,
+  infer Last,
+]
+  ? Rest extends []
+    ? Func<Params, Current extends [] ? Ret : CurriedType<Ret, Current>>
+    : Func<Params, Current extends [] ? Ret : CurriedType<Ret, Current>> &
+        CurriedType<
+          Ret,
+          Rest,
+          [Last, ...(Current extends any[] ? Current : never)]
+        >
+  : never
+
+type Func<Params, Ret> = (
+  ...params: Params extends any[] ? Params : never
+) => Ret
