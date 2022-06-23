2c10735205c48e8a3829df9ff63dcf56406308b1
kizuner-bonely <1003306162@qq.com>
2022-06-23 21:46:07 +0800

    feat: 完成 extreme-readonlyKeys

--- extreme/5-extreme-readonly-keys/template.ts
+++ extreme/5-extreme-readonly-keys/template.ts
@@ -1,1 +1,5 @@
-type GetReadonlyKeys<T> = any
+type GetReadonlyKeys<T extends Record<keyof any, any>> = keyof {
+  [K in keyof T as IsSame<Pick<T, K>, Readonly<{ [P in K]: T[K] }>> extends true
+    ? K
+    : never]: T[K]
+}
