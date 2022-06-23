676b1bb074e19c01fda849352c1bd6a83284cfaa
kizuner-bonely <1003306162@qq.com>
2022-06-23 22:25:02 +0800

    feat: 完成 extreme-slice

--- extreme/216-extreme-slice/template.ts
+++ extreme/216-extreme-slice/template.ts
@@ -1,1 +1,11 @@
-type Slice<Arr, Start, End> = any
+type Slice<
+  Arr extends number[],
+  Start extends number = 0,
+  End extends number = Arr['length'],
+  Ind extends number[] = [],
+  Res extends number[] = [],
+> = Ind['length'] extends End
+  ? Res
+  : GreaterThan<Ind['length'], Start> extends true
+  ? Slice<Arr, Start, End, [...Ind, 0], [...Res, Arr[Start]]>
+  : Slice<Arr, Start, End, [...Ind, 0], Res>
